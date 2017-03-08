c6ac0baedf6be249e671fe530974c7acce454444
Naim Jeem <naim36208@gmail.com>
2017-03-08 13:53:32 +0600

    update

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -2,7 +2,7 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 //Connecto MongoDB
-mongoose.connect('mongodb://<dbuser>:<dbpass>@ds157469.mlab.com:57469/nj-todo');
+mongoose.connect('mongodb://<dbuser>:<dbpass>@ds157469.mlab.com:57469/<dbname>');
 
 //create Schema
 var todoSchema = new mongoose.Schema({
